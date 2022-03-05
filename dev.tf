71f2aee053b0c33e4a699c12c4d0b222778951f6
Olumide <profojah78@gmail.com>
2022-03-05 15:58:55 -0500

    Revert "new changes" changes again
    This reverts commit e4db1669ff95cdd9ac632b3b113ca92fdb887799.

--- dev.tf
+++ dev.tf
@@ -3,27 +3,28 @@
 
 configuration = [
   {
-    application_name : "Ojah-dev",
-    ami : "ami-123456789",
-    no_of_instances : "3",
-    instance_type : "t2.micro",
-    subnet_id : "subnet-0f4f294d8404946eb",
-    vpc_security_group_ids : ["sg-0d15a4cac0567478c","sg-0d8749c35f7439f4e","sg-0d8749c35f7439f3e"]
+    "application_name" : "Ojah-dev",
+    "ami" : "ami-123456789",
+    "no_of_instances" : "3",
+    "instance_type" : "t2.micro",
+    "subnet_id" : "subnet-0f4f294d8404946eb",
+    "vpc_security_group_ids" : ["sg-0d15a4cac0567478c","sg-0d8749c35f7439f4e","sg-0d8749c35f7439f3e"]
   },
   {
-    application_name : "Kenkool-dev",
-    ami : "ami-07412448477447",
-    instance_type : "t2.small",
-    no_of_instances : "2"
-    subnet_id : "subnet-0f4f294d8404946eb"
-    vpc_security_group_ids : ["sg-0d15a4cac0567478c" "sg-0d8749c35f7439567e"]
+    "application_name" : "Kenkool-dev",
+    "ami" : "ami-07412448477447",
+    "instance_type" : "t2.small",
+    "no_of_instances" : "2"
+    "subnet_id" : "subnet-0f4f294d8404946eb"
+    "vpc_security_group_ids" : ["sg-0d15a4cac0567478c" "sg-0d8749c35f7439567e"]
   },
   {
-    application_name : "OpsGrit-dev",
-    ami : "ami-0747bdcabd34c712a",
-    instance_type : "t3.micro",
-    no_of_instances : "1"
-    subnet_id : "subnet-0f4f294d8404946eb"
-    vpc_security_group_ids : ["sg-0d15a4cac0567478c"]
+    "application_name" : "OpsGrit-dev",
+    "ami" : "ami-0747bdcabd34c712a",
+    "instance_type" : "t3.micro",
+    "no_of_instances" : "1"
+    "subnet_id" : "subnet-0f4f294d8404946eb"
+    "vpc_security_group_ids" : ["sg-0d15a4cac0567478c"]
   }
   
+]
